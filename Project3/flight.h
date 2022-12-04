78b2d0354eeae327f1b6c0934d1e8793a191067c
Dravani <42421220+Dravani@users.noreply.github.com>
2022-12-04 17:36:06 -0500

    updated

--- Project3/flight.h
+++ Project3/flight.h
@@ -100,30 +100,30 @@ public:
 
         return pair<string,double>(arr[0], avgDelays[0]);
     }
-    void swap(double *a, double *b) {
-        int t = *a;
-        *a = *b;
-        *b = t;
+    void swap(double *first, double *second) {
+        double temp = *first;
+        *first = *second;
+        *second = temp;
     }
 
-    int partition(double array[], int low, int high) {
-        int pivot = array[high];
-        int i = (low - 1);
-        for (int j = low; j < high; j++) {
+    int partition(double array[], int front, int back) {
+        int pivot = array[back];
+        int i = (front - 1);
+        for (int j = front; j < back; j++) {
             if (array[j] <= pivot) {
                 i++;
                 swap(&array[i], &array[j]);
             }
         }
-        swap(&array[i + 1], &array[high]);
+        swap(&array[i + 1], &array[back]);
         return (i + 1);
     }
 
-    void quickSort(double array[], int low, int high) {
-        if (low < high) {
-            int pi = partition(array, low, high);
-            quickSort(array, low, pi - 1);
-            quickSort(array, pi + 1, high);
+    void quickSort(double array[], int front, int back) {
+        if (front < back) {
+            int k = partition(array, front, back);
+            quickSort(array, front, k - 1);
+            quickSort(array, k + 1, back);
         }
     }
 
